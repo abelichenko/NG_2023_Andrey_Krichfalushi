db86746b7aff319cce8dd86edcce12902e31aa05
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-04 13:54:45 +0300

    Update main.py
    Update code

--- Lection 1/task 4/main.py
+++ Lection 1/task 4/main.py
@@ -1,23 +1,22 @@
 import math
 
-a = input("Choose: \n1) + \n2) - \n3) * \n4) / \n5) √ \n6) ^\n")
+selectAction = input("Choose: \n1) + \n2) - \n3) * \n4) / \n5) √ \n6) ^\n")
 
-if (int(a) < 1 or int(a) > 6):
-    print("You can not do it this way!!!")
-    exit()
+firstValue = input("enter the first number\n")
+secondValue = input("enter second number\n")
 
-b = input("enter the first number\n")
-c = input("enter second number\n")
-
-if(int(a) == 1):
-    print(int(b) + int(c))
-if(int(a) == 2):
-    print(int(b) - int(c))
-if(int(a) == 3):
-    print(int(b) * int(c))
-if(int(a) == 4):
-    print(int(b) / int(c))
-if (int(a) == 5):
-    print(b + "= ", math.sqrt(int(b)), "\n" + c + "= ", math.sqrt(int(c)))
-if (int(a) == 6):
-    print(math.pow(int(b), int(c)))
+match selectAction:
+    case "1":
+        print(int(firstValue) + int(secondValue))
+    case "2":
+        print(int(firstValue) - int(secondValue))
+    case "3":
+        print(int(firstValue) * int(secondValue))
+    case "4":
+        print(int(firstValue) / int(secondValue))
+    case "5":
+        print(firstValue + "= ", math.sqrt(int(firstValue)), "\n" + secondValue + "= ", math.sqrt(int(secondValue)))
+    case "6":
+        print(math.pow(int(firstValue), int(secondValue)))
+    case _:
+        print("Error!")
