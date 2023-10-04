a7ae8eb21cce887d4bce0a73da74693191f4856c
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-04 14:20:33 +0300

    Update main.py
    Update code

--- Lection 1/task 2/main.py
+++ Lection 1/task 2/main.py
@@ -1,3 +1,3 @@
-firstNum = input("Enter first number: ")
-secondNum = input("Enter second number: ")
+firstNum = input("Write me the first number and press Enter: ")
+secondNum = input("write me the second numberand press Enter: ")
 print('The sum of your two numbers:', int(firstNum) + int(secondNum))
