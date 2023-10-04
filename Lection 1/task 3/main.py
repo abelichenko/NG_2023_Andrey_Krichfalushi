8605c665c0466f82ac1c27f8daedd7412e37f4d0
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-04 12:13:45 +0300

    Update main.py
    Update

--- Lection 1/task 3/main.py
+++ Lection 1/task 3/main.py
@@ -1,10 +1,10 @@
-a = input("Choose what you need:\n"
+selectedNum = input("Choose what you need:\n"
           "1) Convert from Celsius to Fahrenheit"
           "\n2) Convert from Fahrenheit to Celsius\n")
 
-b = input("Enter the number of degrees: ")
+value = input("Enter the number of degrees: ")
 
-if(int(a) == 1):
-    print(b + "°C =", int(b) * 9 / 5 + 32, "°F")
-if(int(a) == 2):
-    print(b + "°F =", (int(b) - 32) * 5/9, "°C")
+if(int(selectedNum) == 1):
+    print(b + "°C =", int(value) * 9 / 5 + 32, "°F")
+if(int(selectedNum) == 2):
+    print(b + "°F =", (int(value) - 32) * 5/9, "°C")
