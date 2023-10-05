60f56ce729bf380ba53a9e18ea40fdd78dc63ecd
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-05 13:52:04 +0300

    Update main.py
    update code

--- Lection 1/task 4/main.py
+++ Lection 1/task 4/main.py
@@ -2,21 +2,21 @@ import math
 
 selectAction = input("Choose: \n1) + \n2) - \n3) * \n4) / \n5) √ \n6) ^\n")
 
-firstValue = input("enter the first number\n")
-secondValue = input("enter second number\n")
+firstValue = float(input("enter the first number\n"))
+secondValue = float(input("enter second number\n"))
 
 match selectAction:
     case "1":
-        print(int(firstValue) + int(secondValue))
+        print(firstValue + secondValue)
     case "2":
-        print(int(firstValue) - int(secondValue))
+        print(firstValue - secondValue)
     case "3":
-        print(int(firstValue) * int(secondValue))
+        print(firstValue * secondValue)
     case "4":
-        print(int(firstValue) / int(secondValue))
+        print(firstValue / secondValue)
     case "5":
-        print(firstValue + "= ", math.sqrt(int(firstValue)), "\n" + secondValue + "= ", math.sqrt(int(secondValue)))
+        print(firstValue, "= ", math.sqrt(firstValue), "\n" , secondValue, "= ", math.sqrt(secondValue))
     case "6":
-        print(math.pow(int(firstValue), int(secondValue)))
+        print(math.pow(firstValue, secondValue))
     case _:
         print("Error!")
