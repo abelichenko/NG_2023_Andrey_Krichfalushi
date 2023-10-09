2eefb02aecff655095129a4380fd6c57ebca92c0
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-09 11:02:10 +0300

    Update main.py
    Small changes to the code

--- Lection 1/task 3/main.py
+++ Lection 1/task 3/main.py
@@ -1,10 +1,26 @@
-selectedNum = int(input("Choose what you need:\n"
-          "1) Convert from Celsius to Fahrenheit"
-          "\n2) Convert from Fahrenheit to Celsius\n"))
+def verifNum(num): #Valid selection check
+    if(num != 1 and num != 2):
+        print("You can't do that, you RAGENT!")
+        return False
+    return True
 
-value = int(input("Enter the number of degrees: "))
+def actionSelection(): #Please enter the user's number for him to select an action
+    selectedNum = int(input("Choose what you need:\n"
+              "1) Convert from Celsius to Fahrenheit"
+              "\n2) Convert from Fahrenheit to Celsius\n"))
+    if(verifNum(selectedNum)): #We send his choice for verification
+        return selectedNum
+    else:
+        main()
 
-if(selectedNum == 1):
-    print(value, "°C =", value * 9 / 5 + 32, "°F")
-if(selectedNum == 2):
-    print(value, "°F =", (value - 32) * 5/9, "°C")
+def main():
+    selectedNum = actionSelection()
+    
+    value = int(input("Enter the number of degrees: "))
+    
+    if(selectedNum == 1):
+        print(value, "°C =", value * 9 / 5 + 32, "°F")
+    if(selectedNum == 2):
+        print(value, "°F =", (value - 32) * 5/9, "°C")
+        
+main()
