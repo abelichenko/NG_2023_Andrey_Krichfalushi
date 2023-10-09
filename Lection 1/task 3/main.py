a9ae1b28d5b74ec158316f0e49cff70c5de72fa0
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-09 12:51:12 +0300

    Update main.py
    Remove recursion

--- Lection 1/task 3/main.py
+++ Lection 1/task 3/main.py
@@ -1,21 +1,14 @@
-def actionSelection(): #Please enter the user's number for him to select an action
-    selectedNum = int(input("Choose what you need:\n"
-              "1) Convert from Celsius to Fahrenheit"
-              "\n2) Convert from Fahrenheit to Celsius\n"))
-    if(selectedNum == 1 or selectedNum == 2): 
-        return selectedNum
-    else:
-        print("invalid value")
-        main()
+selectedNum = int(input("Choose what you need:\n"
+          "1) Convert from Celsius to Fahrenheit"
+          "\n2) Convert from Fahrenheit to Celsius\n"))
+if(selectedNum != 1 and selectedNum != 2): 
+    print("invalid value")
+    exit()
 
-def main():
-    selectedNum = actionSelection()
+value = int(input("Enter the number of degrees: "))
+
+if(selectedNum == 1):
+    print(value, "°C =", value * 9 / 5 + 32, "°F")
+if(selectedNum == 2):
+    print(value, "°F =", (value - 32) * 5/9, "°C")
     
-    value = int(input("Enter the number of degrees: "))
-    
-    if(selectedNum == 1):
-        print(value, "°C =", value * 9 / 5 + 32, "°F")
-    if(selectedNum == 2):
-        print(value, "°F =", (value - 32) * 5/9, "°C")
-        
-main()
