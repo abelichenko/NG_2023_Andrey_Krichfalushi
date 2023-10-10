365b1dd17ffb3c0756bd24bcb473526e80153d9e
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-10 10:55:26 +0300

    Update main.py
    added match

--- Lection 1/task 3/main.py
+++ Lection 1/task 3/main.py
@@ -1,14 +1,13 @@
 selectedNum = int(input("Choose what you need:\n"
           "1) Convert from Celsius to Fahrenheit"
           "\n2) Convert from Fahrenheit to Celsius\n"))
-if(selectedNum != 1 and selectedNum != 2): 
-    print("invalid value")
-    exit()
 
 value = int(input("Enter the number of degrees: "))
-
-if(selectedNum == 1):
-    print(value, "°C =", value * 9 / 5 + 32, "°F")
-if(selectedNum == 2):
-    print(value, "°F =", (value - 32) * 5/9, "°C")
     
+match selectedNum:
+    case 1:
+        print(value, "°C =", value * 9 / 5 + 32, "°F")
+    case 2:
+        print(value, "°F =", (value - 32) * 5/9, "°C")
+    case _:    
+        print("Wrong choice of action. Can be 1 or 2.")
