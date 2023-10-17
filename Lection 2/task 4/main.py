0b17a9ea6634c2d2ae7faabbb8b32eae1c46ec37
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-17 17:31:57 +0300

    Update main.py
    Update code

--- Lection 2/task 4/main.py
+++ Lection 2/task 4/main.py
@@ -1,10 +1,6 @@
 vowels = ["A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y", " ", "-", ".", ",", "`", ":", "(", ")", ";", "[",
           "]", "{", "}"]
 string = input("Go print! ")
-output = ""
-
 for letter in string:
-    if letter in vowels:
-        output = output + letter
-
-print(output)   
+    if(letter in vowels):
+        print(letter)
