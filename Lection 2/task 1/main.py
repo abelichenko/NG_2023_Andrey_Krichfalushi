aca6258f1b31465db094000aa2529d729472ba1f
abelichenko <62173389+abelichenko@users.noreply.github.com>
2023-10-17 17:25:38 +0300

    Update main.py

--- Lection 2/task 1/main.py
+++ Lection 2/task 1/main.py
@@ -1,16 +1,2 @@
-lst = input("Write whatever you want: ")
-archive = []
-blackList = []
-
-for element in lst:
-    if element not in archive:
-        archive.append(element)
-    elif element not in blackList:
-        blackList.append(element)
-
-for element in blackList:
-    if element in archive:
-        archive.remove(element)
-
-for element in archive:
-    print(element)
+row = input("Enter your row: ")
+print("unique symbols: ", set(row.split()))
